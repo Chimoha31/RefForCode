d18050adcb5d3594c324b88962678183ee9b3a42
Chimoha31 <mokochii1108@gmail.com>
2022-07-10 13:44:30 -0700

    Done

--- 01Redux/basic/src/index.js
+++ 01Redux/basic/src/index.js
@@ -2,6 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {createStore} from 'redux';
+
+// actions -> increment, decrement
+const increment = () => {
+  return {
+    type: "INCREMENT",
+  }
+}
+
+const decrement = () => {
+  return {
+    type: "DECREMENT",
+  }
+}
+
+// reducer -> action + previous state = update new state
+const counterReducer = (state = 0, action) => {
+  switch(action.type) {
+    case "INCREMENT":
+      return state + 1;
+      case "DECREMENT":
+        return state - 1;
+  }
+}
+
+// store
+let store = createStore(counterReducer);
+
+// subscribe() = output a new state on console (redux method)
+store.subscribe(() => console.log(store.getState()));
+
+// dispatch
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(increment());
+store.dispatch(decrement());
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
